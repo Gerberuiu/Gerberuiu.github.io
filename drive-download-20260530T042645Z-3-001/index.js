6f22f3ac521b6a2b33edf5c4fe3c6c21fd2da794
Gerberuiu <gerbertbatres55@gmail.com>
2026-05-29 22:41:41 -0600

    Delete drive-download-20260530T042645Z-3-001 directory

--- drive-download-20260530T042645Z-3-001/index.js
+++
@@ -1,5 +0,0 @@
-const toggle = document.querySelector(".toggle")
-const navMenu = document.querySelector(".nav-menu")
-toggle.addEventListener("click",()=>{
-    navMenu.classList.toggle("nav-menvi");
-});
